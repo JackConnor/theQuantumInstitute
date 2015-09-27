200255ba6ef5582e24d4a4b95145b07783f061c4
Jack Connor <jack.connor83@gmail.com>
2015-09-27 13:32:13 -0700

    html framework completed with all views, though no design or js logic

--- theQuantumInstitute.js
+++ theQuantumInstitute.js
@@ -2,22 +2,28 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+//begin navbar helpers/events
+  Template.navbar.helpers({
+
+  });
+
+  Template.navbar.events({
+
+  });
+
+//begin listview helpers/events
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+
   });
 
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    console.log('whatup dawg');
   });
 }
