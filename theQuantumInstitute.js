92adac5686dd7b72729637daecb02e6f900d4389
Jack Connor <jack.connor83@gmail.com>
2015-09-27 14:24:12 -0700

    logic installed for SPA-style pageview switches

--- theQuantumInstitute.js
+++ theQuantumInstitute.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault('listingOn', true);
 
 //begin navbar helpers/events
   Template.navbar.helpers({
@@ -8,17 +8,47 @@ if (Meteor.isClient) {
   });
 
   Template.navbar.events({
-  
+    'click .seeAll': function(){
+      console.log('testing');
+      Session.set('listingOn', true)
+    },
+    'click .contact': function(){
+      console.log('testing');
+      Session.set('listingOn', false)
+    }
+
   });
 
-//begin listview helpers/events
-  Template.hello.helpers({
+// begin bodyContainer helpers/events
+  Template.bodyContainer.helpers({
+    // listing == + will give us the listview, otherwise the contact page
+    listing: function(){
+      return Session.get('listingOn');
+    }
 
   });
 
-  Template.hello.events({
+  Template.bodyContainer.events({
 
   });
+
+  // begin listview helpers/events
+  Template.listview.helpers({
+
+  })
+
+  Template.listview.events({
+
+  })
+
+  // begin contact helpers/events
+  Template.contact.helpers({
+
+  })
+
+  Template.contact.events({
+
+  })
 }
 
 if (Meteor.isServer) {
